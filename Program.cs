9bd807d81678395a5f9021f0fc493bc24918d8fb
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-09 10:20:01 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_6_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
