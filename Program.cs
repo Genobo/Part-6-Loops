cbb56d90f1fa0d8cc39a26dfd7aee6ac66033272
Gabriel Lindsay <339516205@GoTVDSB.ca>
2023-11-09 11:00:00 -0500

    Started prompter

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter 1 to go to the prompter.");
+            Console.ReadLine();
+            Prompter();
+        }
+
+        public static void Prompter()
+        {
+            int minNum;
+            int maxNum;
+            Console.WriteLine("Please enter a number with a minimum value and maximum value.");
+            Console.WriteLine("Please enter a minimum value:");
+            minNum = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter a maximum value:");
+            maxNum = Convert.ToInt32(Console.ReadLine());
         }
     }
 }
